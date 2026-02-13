171dc7d16acc52bddf586c1c626f0a8a43dfe540
Grimspook <46845957+GrimSpook@users.noreply.github.com>
2026-02-13 16:10:48 +0100

    name change: User - Player

--- shared/types/Models.types.ts
+++ shared/types/Models.types.ts
@@ -1,8 +1,8 @@
 /**
  * Re-export Prisma Models to avoid circular dependencies between backend and frontend
  */
-import type { User, Scoreboard, Game } from "../../backend/generated/prisma/client.ts";
+import type { Player, Scoreboard, Game } from "../../backend/generated/prisma/client.ts";
 
-export type PrismaUser = User;
+export type PrismaPlayer = Player;
 export type PrismaScoreboard = Scoreboard;
 export type PrismaGame = Game;
