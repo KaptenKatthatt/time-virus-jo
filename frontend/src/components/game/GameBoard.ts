41e905ae6d45e402f685bd35756172e85eb5e261
Jonas Olson <jonas.olson@gmail.com>
2026-02-16 19:50:51 +0100

    Refactored Gameboard

--- frontend/src/components/game/GameBoard.ts
+++ frontend/src/components/game/GameBoard.ts
@@ -3,23 +3,34 @@ import type { Socket } from "socket.io-client";
 
 export default function GameBoard(socket: Socket<ServerToClientEvents, ClientToServerEvents>) {
 	console.log("GameBoard rendering");
+	const gameBoard = document.createElement("div");
 
-	const render = () => {
-		const gameBoard = document.createElement("div");
-		gameBoard.className = "game-board";
+	const handleVirusClick = (virus: HTMLButtonElement) => {
+		virus.remove();
+		//TODO Send player timestamp to server
+		//TODO Check if 10 turns
+		//TODO YES? Go to GameOver. NO? Start new round
+	};
 
+	const setupSocketListeners = () => {
 		socket.on("game:virus", (payload) => {
 			const virus = document.createElement("button");
 			virus.className = `virus x${payload.x + 1} y${payload.y + 1}`;
-			virus.addEventListener("click", () => {});
+
+			virus.addEventListener("click", () => {
+				handleVirusClick(virus);
+			});
 
 			setTimeout(() => {
 				gameBoard.appendChild(virus);
 			}, payload.delay);
 		});
-
-		return gameBoard;
 	};
 
+	const render = () => {
+		gameBoard.className = "game-board";
+		setupSocketListeners();
+		return gameBoard;
+	};
 	return render();
 }
