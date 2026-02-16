9dca6f70c064946b43a0c05c0eaebab2e19d16ff
Jonas Olson <jonas.olson@gmail.com>
2026-02-16 19:04:25 +0100

    wip: Refactoring Gameboard logic

--- backend/src/services/virus.service.ts
+++ backend/src/services/virus.service.ts
@@ -3,7 +3,7 @@
 export const summonVirus = () => {
 	const randX = Math.floor(Math.random() * 10);
 	const randY = Math.floor(Math.random() * 10);
-	const delay = Math.floor(Math.random() * 5);
+	const delay = Math.floor(Math.random() * 5000);
 	return { x: randX, y: randY, delay: delay };
 };
 
--- frontend/src/components/game/GameBoard.ts
+++ frontend/src/components/game/GameBoard.ts
@@ -1,55 +1,25 @@
-import type { GameId, VirusPayload } from "@shared/types/payloads.types";
 import type { ClientToServerEvents, ServerToClientEvents } from "@shared/types/SocketEvents.types";
 import type { Socket } from "socket.io-client";
 
-function GameBoard(socket: Socket<ServerToClientEvents, ClientToServerEvents>, gameId: GameId) {
-	const gameBoard = document.createElement("div");
-	gameBoard.className = "game-board";
+export default function GameBoard(socket: Socket<ServerToClientEvents, ClientToServerEvents>) {
 	console.log("GameBoard rendering");
 
-	// Generate squares with two loops
-	for (let x = 0; x <= 9; x++) {
-		for (let y = 0; y <= 9; y++) {
-			const cell = document.createElement("div");
-			cell.classList.add("cell");
-			cell.dataset.x = String(x);
-			cell.dataset.y = String(y);
-			cell.id = `cell-${x}-${y}`;
-			gameBoard.appendChild(cell);
-		}
-	}
+	const render = () => {
+		const gameBoard = document.createElement("div");
+		gameBoard.className = "game-board";
 
-	// TODO Refactor into SASS classes
+		socket.on("game:virus", (payload) => {
+			const virus = document.createElement("button");
+			virus.className = `virus x${payload.x + 1} y${payload.y + 1}`;
+			virus.addEventListener("click", () => {});
 
-	// row i / i+1. For loop i SASS?
-	// TODO Receive virus coords and delay
-
-	socket.on("game:virus", (virus) => {
-		const virusCell = gameBoard.querySelector<HTMLDivElement>(`#cell-${virus.x}-${virus.y}`)!;
-
-		if (virusCell) {
-			virusCell.classList.add("virus");
-		}
-
-		virusCell.addEventListener("click", (e) => {
-			const clickedCell = e.target as HTMLDivElement;
-
-			// Log to console coords of virus
-			if (clickedCell.classList.contains("cell")) {
-				console.log(clickedCell.dataset.x, clickedCell.dataset.y);
-			}
-			if (clickedCell.classList.contains("virus")) {
-				clickedCell.classList.remove("virus");
-			}
+			setTimeout(() => {
+				gameBoard.appendChild(virus);
+			}, payload.delay);
 		});
-		const currentVirus = gameBoard.querySelector<HTMLDivElement>(".virus");
 
-		if (currentVirus) {
-			currentVirus.classList.remove("virus");
-		}
-	});
+		return gameBoard;
+	};
 
-	return gameBoard;
+	return render();
 }
-
-export default GameBoard;
