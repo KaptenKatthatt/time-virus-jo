87a81743302dbddd7a66df2e3b2dd6879bbe1a0a
Sandra Coco <sandrac94@hotmail.com>
2026-02-17 16:45:46 +0100

    added inline style for now to make modal visable

--- frontend/src/components/Modal.ts
+++ frontend/src/components/Modal.ts
@@ -3,6 +3,18 @@ export function Modal(content: HTMLElement) {
         const wrapper = document.createElement("div");
         wrapper.className = "modal fade show";
 
+        wrapper.style.position = "fixed";
+        wrapper.style.top = "0";
+        wrapper.style.left = "0";
+        wrapper.style.width = "100vw";
+        wrapper.style.height = "100vh";
+        wrapper.style.display = "flex";
+        wrapper.style.justifyContent = "center";
+        wrapper.style.alignItems = "center";
+        wrapper.style.background = "rgba(0,0,0,0.5)";
+        wrapper.style.zIndex = "2000";
+
+
         const dialog = document.createElement("div");
         dialog.className = "modal-dialog modal-dialog-centered";
 
