a1566f92eeb55f118a741fb914aeaa543cf0587b
Sandra Coco <sandrac94@hotmail.com>
2026-02-17 09:41:35 +0100

    created a component to render virus to ui and handling x,y positions

---
+++ frontend/src/components/game/Virus.ts
@@ -0,0 +1,22 @@
+import virusImg from "../../assets/imgs/virusv1.svg";
+
+export function Virus(x: number, y: number, cb?: (event?: PointerEvent) => void) {
+    const listener = (event: PointerEvent) => {
+		if (cb) {
+			cb(event);
+		}
+	};
+
+    const render = () => {
+        const virus = document.createElement("img");
+
+        virus.src = virusImg;
+        virus.className = `virus x${x} y${y}`;
+
+        virus.addEventListener("click", listener);
+
+        return virus;
+    };
+
+    return render();
+}
