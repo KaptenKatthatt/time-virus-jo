148f3d3a9933d7c287a61d4980d463bc3e2641b1
Grimspook <46845957+GrimSpook@users.noreply.github.com>
2026-02-17 13:35:11 +0100

    updated buttn style

--- frontend/src/components/Button.ts
+++ frontend/src/components/Button.ts
@@ -8,7 +8,7 @@ export default function Button(text: string, cb?: (event?: PointerEvent) => void
 	const render = () => {
 		const button = document.createElement("button");
 
-		button.className = "btn btn-primary";
+		button.className = "btn btn-border-img";
 
 		button.innerText = text;
 		button.addEventListener("click", listener);
