1c39934f342e614b3bf00ec8dc1b98a204423d47
Grimspook <46845957+GrimSpook@users.noreply.github.com>
2026-02-18 10:28:55 +0100

    added game ui styling to the modals

--- frontend/src/components/Modal.ts
+++ frontend/src/components/Modal.ts
@@ -19,7 +19,8 @@ export function Modal(content: HTMLElement) {
         dialog.className = "modal-dialog modal-dialog-centered";
 
         const textbox = document.createElement("div");
-        textbox.className = "modal-content p-2 text-center";
+        textbox.className = "modal-content p-2 text-center modal-border-img";
+
 
         textbox.appendChild(content);
         dialog.appendChild(textbox);
@@ -27,6 +28,6 @@ export function Modal(content: HTMLElement) {
 
         return wrapper;
     }
-    
+
     return render();
 }
