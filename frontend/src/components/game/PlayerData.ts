9b790ff5b4038e90e92ee8fa585769ee5c2fffb2
Jonas Olson <jonas.olson@gmail.com>
2026-02-18 20:04:26 +0100

    wip: Displays name and score after first round, not before yet. Refactored PlayerData component.

--- frontend/src/components/game/PlayerData.ts
+++ frontend/src/components/game/PlayerData.ts
@@ -1,17 +1,22 @@
+export interface PlayerUI {
+	element: HTMLDivElement;
+	update: (name: string, score: number) => void;
+}
+
 export default function PlayerData(
 	playerName: string,
 	playerScore: number,
 	// playerBestTime: number,
-): HTMLDivElement {
+): PlayerUI {
 	const container: HTMLDivElement = document.createElement("div");
 	container.className = "col-auto player-info d-flex flex-column align-items-center";
 
 	const nameEl = document.createElement("span");
-	nameEl.className = `display-6`;
+	nameEl.className = "display-6";
 	nameEl.textContent = playerName;
 
 	const scoreEl = document.createElement("span");
-	scoreEl.className = `display-2`;
+	scoreEl.className = "display-2";
 	scoreEl.textContent = String(playerScore);
 
 	// const bestTimeHeadingEl = document.createElement("span");
@@ -22,10 +27,15 @@ export default function PlayerData(
 	// bestTimeEl.className = `player-${playerNbr}-best-time`;
 	// bestTimeEl.textContent = `${playerBestTime}s`;
 
-	container.appendChild(nameEl);
-	container.appendChild(scoreEl);
+	container.append(nameEl, scoreEl);
 	// container.appendChild(bestTimeHeadingEl);
 	// container.appendChild(bestTimeEl);
 
-	return container;
+	return {
+		element: container,
+		update: (newName: string, newScore: number) => {
+			nameEl.textContent = newName;
+			scoreEl.textContent = String(newScore);
+		},
+	};
 }
--- frontend/src/pages/game.ts
+++ frontend/src/pages/game.ts
@@ -2,17 +2,39 @@ import type { Socket } from "socket.io-client";
 import GameBoard from "../components/game/GameBoard";
 import type { ClientToServerEvents, ServerToClientEvents } from "@shared/types/SocketEvents.types";
 import type { GamePayload } from "@shared/types/payloads.types";
+import PlayerData from "../components/game/PlayerData";
 
-interface PlayerPayload {
-	name: string;
-	id: string;
-	score: number;
-}
+// interface PlayerPayload {
+// 	name: string;
+// 	id: string;
+// 	score: number;
+// }
 
-export default function Game(
-	socket: Socket<ServerToClientEvents, ClientToServerEvents>,
-	gameId: GameId = "",
-) {
+/**
+ * Game page with score and gameboard
+ * @param socket
+ */
+export default function Game(socket: Socket<ServerToClientEvents, ClientToServerEvents>) {
+	// Static containers
+	const container = document.createElement("div");
+	const row = document.createElement("div");
+
+	// Dynamic containers
+	const p1 = PlayerData("P1", 0);
+	const p2 = PlayerData("P2", 0);
+
+	const aside = document.createElement("aside");
+	const game = document.createElement("div");
+	// let playerOneEl = PlayerData("P1", 0);
+	// let playerTwoEl = PlayerData("P2", 0);
+	const timeCol = document.createElement("div");
+	const title = document.createElement("div");
+
+	const p1Name = document.createElement("span");
+	const p1Score = document.createElement("span");
+	const p2Name = document.createElement("span");
+	const p2Score = document.createElement("span");
+
 	// TODO: subscribe to socket events to keep UI (scores, time, round) in sync
 
 	//TODO Refactor GamePayload into GameUpdate that contains only score update and round nbr
@@ -31,39 +53,35 @@ export default function Game(
 				fastest_Time,
 			} = payload;
 
-			playerOneEl = PlayerData(player_one_name, player_one_score);
-			playerTwoEl = PlayerData(player_two_name, player_two_score);
-
-			row.replaceChild(playerOneEl, row.firstChild!);
-			row.replaceChild(playerTwoEl, row.lastChild!);
+			p1.update(player_one_name, player_one_score);
+			p2.update(player_two_name, player_two_score);
+			// p1Name.innerText = player_one_name ?? "";
+			// p1Score.innerText = player_one_score?.toString() ?? "";
+			// p2Name.innerText = player_two_name ?? "";
+			// p2Score.innerText = player_two_score?.toString() ?? "";
 		}
 	});
 
-	let playerOneEl = PlayerData("P1", 0);
-	let playerTwoEl = PlayerData("P2", 0);
-	const timeCol = document.createElement("div");
-	const row = document.createElement("div");
-
-
 	const render = () => {
-		const game = document.createElement("div");
-		game.className =
-			"game-container container d-flex flex-column justify-content-around vh-100";
-		const root = document.createElement("div");
-		root.className = "game-grid justify-content-center gap-4 align-items-between h-100";
+		container.className = "game-grid justify-content-center gap-4 align-items-between h-100";
 
 		// Score / header row
 		row.className = "row score-row justify-content-between";
+		// row.className = "gameboard-grid";
 
+		game.className =
+			"game-container container d-flex flex-column justify-content-around vh-100";
+
+		// Countup timer
 		timeCol.className = "col-auto time-round d-flex flex-column align-items-center";
 		timeCol.innerHTML = `
 			<span class="time display-1">00:00</span>
 			<span class="round-heading">Round</span>
-			<span class="round display-3">1
-		const aside = document.createElement("aside");
+			<span class="round display-3">1`;
+
+		// Aside
 		aside.className = "d-flex flex-xl-column gap-4 justify-content-evenly p-5";
 
-		const title = document.createElement("div");
 		title.className =
 			"title-span p-4 d-flex bg-dark flex-column justify-content-center align-items-center border-img-dark";
 		title.innerHTML = `
@@ -74,71 +92,65 @@ export default function Game(
 			</span>
 		`;
 
-		row.appendChild(playerOneEl);
-		row.appendChild(timeCol);
-		row.appendChild(playerTwoEl);
-
-		game.appendChild(row);
-
-		const board = GameBoard(socket);
-		game.appendChild(board);
-
-		// Score / header row
-		const row = document.createElement("div");
-		row.className = "gameboard-grid";
-
-		const score = Score(player1.score, player2.score);
-		const playerOne = PlayerScore(player1, "id1");
-		const playerTwo = PlayerScore(player2, "id1");
+		// const score = Score(player1.score, player2.score);
+		// const playerOne = PlayerScore(player1, "id1");
+		// const playerTwo = PlayerScore(player2, "id1");
 
 		aside.appendChild(title);
-		aside.appendChild(score);
-		aside.appendChild(playerOne);
-		aside.appendChild(playerTwo);
+		// aside.appendChild(score);
+		// aside.appendChild(playerOne);
+		// aside.appendChild(playerTwo);
+		// row.appendChild(playerOneEl);
+		// row.appendChild(timeCol);
+		// row.appendChild(playerTwoEl);
 
-		root.appendChild(board);
-		root.appendChild(aside);
+		row.append(p1.element, timeCol, p2.element);
 
-		return game;
+		container.appendChild(aside);
+
+		container.appendChild(row);
+
+		const board = GameBoard(socket);
+		container.appendChild(board);
+		return container;
 	};
 
 	return render();
 }
 
-function Score(scoreOne: number, scoreTwo: number) {
-	const render = () => {
-		const root = document.createElement("div");
-		root.className =
-			"d-flex justify-content-center align-items-center display-5 bg-dark gap-4 border-img-dark p-4";
+// function Score(scoreOne: number, scoreTwo: number) {
+// 	const render = () => {
+// 		container.className =
+// 			"d-flex justify-content-center align-items-center display-5 bg-dark gap-4 border-img-dark p-4";
 
-		root.innerHTML = `
-			<span >${scoreOne}</span>
-			<span>-</span>
-			<span>${scoreTwo}</span>
-		`;
+// 		container.innerHTML = `
+// 			<span >${scoreOne}</span>
+// 			<span>-</span>
+// 			<span>${scoreTwo}</span>
+// 		`;
 
-		return root;
-	};
-	return render();
-}
+// 		return container;
+// 	};
+// 	return render();
+// }
 
-function PlayerScore(player: PlayerPayload, socketId: string) {
-	const playerId = player.id;
-	const name = player.name;
+// function PlayerScore(player: PlayerPayload, socketId: string) {
+// 	const playerId = player.id;
+// 	const name = player.name;
 
-	const render = () => {
-		const div = document.createElement("div");
-		const isMe = socketId === playerId ? "text-primary" : "";
+// 	const render = () => {
+// 		const div = document.createElement("div");
+// 		const isMe = socketId === playerId ? "text-primary" : "";
 
-		div.className =
-			"d-flex justify-content-evenly flex-column bg-dark align-items-center p-4 border-img-dark";
+// 		div.className =
+// 			"d-flex justify-content-evenly flex-column bg-dark align-items-center p-4 border-img-dark";
 
-		div.innerHTML = `
-			<span class="${isMe} display-lg-5 display-6">${name}</span>
-			<span class="fs-2">00:00</span>
-		`;
+// 		div.innerHTML = `
+// 			<span class="${isMe} display-lg-5 display-6">${name}</span>
+// 			<span class="fs-2">00:00</span>
+// 		`;
 
-		return div;
-	};
-	return render();
-}
+// 		return div;
+// };
+// 	return render();
+// }
