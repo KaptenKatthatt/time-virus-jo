e0bc0ad16039c025cfbe803dd0bd5fe2d27be136
Jonas Olson <jonas.olson@gmail.com>
2026-02-23 07:45:36 +0100

    Fix createPlayer function to use create instead of upsert

--- backend/src/services/player.service.ts
+++ backend/src/services/player.service.ts
@@ -1,20 +1,11 @@
 import { type Prisma } from "../../generated/prisma/client.ts";
 import { prisma } from "../lib/prisma.ts";
 
-/**
- * Create user
- *@param data Player information
- * @returns {Player} Player
- */
-
 export const createPlayer = async (data: Prisma.PlayerCreateInput) => {
-	return await prisma.player.upsert({
-		where: {
-			id: data.id as string,
-		},
-		update: {
+	return await prisma.player.create({
+		data: {
+			id: data.id,
 			name: data.name,
 		},
-		create: data,
 	});
 };
