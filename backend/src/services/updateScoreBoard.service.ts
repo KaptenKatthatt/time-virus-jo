319dc927fd7438d2a1f6336c24323750641ad2ab
Sandra Coco <sandrac94@hotmail.com>
2026-02-23 11:59:49 +0100

    created services to create and find data for scoreboard and sent it in socket controller

---
+++ backend/src/services/updateScoreBoard.service.ts
@@ -0,0 +1,25 @@
+import { ScoreBoardPayload } from "@shared/types/payloads.types.ts";
+import { prisma } from "../lib/prisma.ts";
+
+export const createScoreboard = async (payload: ScoreBoardPayload) => {
+    return await prisma.scoreboard.create({
+        data: {
+            name: payload.name,
+            player_one_name: payload.player_one_name,
+            player_two_name: payload.player_two_name,
+            player_one_score: payload.player_one_score,
+            player_two_score: payload.player_two_score,
+        }
+    });
+}
+
+export const getScoreboard = async () => {
+    return await prisma.scoreboard.findMany({
+        orderBy: {
+            id: "desc",
+        },
+        omit: {
+            id: true,
+        }
+    });
+}
