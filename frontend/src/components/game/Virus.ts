6ef1e2ba7adf270abd4dbd39e8023c370a62f371
Jonas Olson <jonas.olson@gmail.com>
2026-02-24 20:56:00 +0100

    Can't touch this

--- frontend/src/components/game/Virus.ts
+++ frontend/src/components/game/Virus.ts
@@ -1,22 +1,23 @@
 import virusImg from "../../assets/imgs/virusv1.svg";
 
 export function Virus(x: number, y: number, cb?: (event?: PointerEvent) => void) {
-    const listener = (event: PointerEvent) => {
+	const listener = (event: PointerEvent) => {
 		if (cb) {
 			cb(event);
 		}
 	};
 
-    const render = () => {
-        const virus = document.createElement("img");
+	const render = () => {
+		const virus = document.createElement("img");
 
-        virus.src = virusImg;
-        virus.className = `virus x${x} y${y}`;
+		virus.src = virusImg;
+		virus.className = `virus x${x} y${y}`;
+		virus.draggable = false;
 
-        virus.addEventListener("click", listener);
+		virus.addEventListener("click", listener);
 
-        return virus;
-    };
+		return virus;
+	};
 
-    return render();
+	return render();
 }
